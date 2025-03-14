c593b54f358d6362be3dba7d04d65297d013495b
christaay <145792622+christaay@users.noreply.github.com>
2025-03-14 19:55:59 -0400

    Organised ice files

---
+++ Assignment1/app/app.js
@@ -0,0 +1,8 @@
+// Name: Akshay Mathew
+// Course: INFT 2202
+// Date: 2025-02-15
+// Description: This is a page for my shop.
+
+
+// Set the current year in the footer
+document.getElementById('copyrightYear').textContent = new Date().getFullYear();
